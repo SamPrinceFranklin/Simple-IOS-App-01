091c992398d4c2f5b7528705c5f8ffe63c8ee670
Sam Prince Franklin <73087774+SamPrinceFranklin@users.noreply.github.com>
2021-09-20 14:58:36 +0530

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sam Prince K on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
